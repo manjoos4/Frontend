d0f99b4cdbc1ecba3a1c557e6e937f2bb761e1c4
manjoos4 <manjoos4@gmail.com>
2021-12-01 01:25:19 +0530

    portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Frontend';
+  title = 'PORTFOLIO';
+  // myImage:string="assets/images/image.jpg"
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { SamplesComponent } from './samples/samples.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServicesComponent } from './services/services.component';
+import { TestimonyComponent } from './testimony/testimony.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    HomeComponent,
+    SamplesComponent,
+    ContactComponent,
+    FooterComponent,
+    ServicesComponent,
+    TestimonyComponent,
   ],
   imports: [
     BrowserModule,
